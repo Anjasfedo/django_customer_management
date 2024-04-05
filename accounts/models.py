e1d0faeff9fa05757b06f85abfc2575e4ad874d1
AnjasFedo <fedoaf98@gmail.com>
2024-04-05 12:48:53 +0700

    Database Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    email = models.CharField(max_length=50, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out door', 'Out door')
+    )
+    
+    name = models.CharField(max_length=50, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=50, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+    # customer
+    # product
+    status = models.CharField(max_length=50, null=True, choices=STATUS)
+    created_at = models.DateTimeField(
+        auto_now_add=True, null=True)
